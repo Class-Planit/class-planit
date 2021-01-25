0ed22e14c834c82761509c14e82d80278e345662
audreyakwenye <44106290+audreyakwenye@users.noreply.github.com>
2021-01-25 00:24:21 -0600

    better

--- planit/topic_matching.py
+++ planit/topic_matching.py
@@ -37,7 +37,7 @@ from bs4 import BeautifulSoup
 from nltk.corpus import stopwords
 stop_words = stopwords.words('english')
 import re
-import language_tool_python
+
 stop_words.extend(['The', 'students', 'learn'])
 count_vect = CountVectorizer()
 porter = PorterStemmer()
@@ -48,12 +48,9 @@ wikipedia.set_rate_limiting(True)
 stop_words = ['i', "'", '!', '.', ':', ',', '[', ']', '(', ')', '?', "'see", "see", '...',  'student', 'learn', 'objective', 'students', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', "you're", "you've", "you'll", "you'd", 'your', 'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', "she's", 'her', 'hers', 'herself', 'it', "it's", 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', "that'll", 'these', 'those', 'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does', 'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through', 'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more', 'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so', 'than', 'too', 'very', 's', 't', 'can', 'will', 'just', 'don', "don't", 'should', "should've", 'now', 'd', 'll', 'm', 'o', 're', 've', 'y', 'ain', 'aren', "aren't", 'couldn', "couldn't", 'didn', "didn't", 'doesn', "doesn't", 'hadn', "hadn't", 'hasn', "hasn't", 'haven', "haven't", 'isn', "isn't", 'ma', 'mightn', "mightn't", 'mustn', "mustn't", 'needn', "needn't", 'shan', "shan't", 'shouldn', "shouldn't", 'wasn', "wasn't", 'weren', "weren't", 'won', "won't", 'wouldn', "wouldn't"]
 
 TAG_RE = re.compile(r'<[^>]+>')
-tool = language_tool_python.LanguageTool('en-US') 
 
-def check_grammar(sentence):
-    i = 0
-    match = tool.correct(sentence) 
-    return(match)
+
+
 
 
 
@@ -505,7 +502,7 @@ def genQuestion(line):
     elif all(key in bucket for key in l13): #'NN', 'VBZ' in sentence.
         question = 'What' + ' ' + line.words[bucket['VBZ']] + ' ' + line.words[bucket['NN']] + '?'
 
-    question = check_grammar(question)
+    
     if question:
         question_match, create = mainQuestion.objects.get_or_create(question=question, answer=line) 
     else:
