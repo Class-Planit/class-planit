75a66f27b21535e705e5bb3d2c8a9d7777f9a3f5
audreyakwenye <44106290+audreyakwenye@users.noreply.github.com>
2021-05-23 19:46:37 -0500

    better

--- planit/get_openai.py
+++ planit/get_openai.py
@@ -2,16 +2,11 @@ import os
 import openai
 from decouple import config, Csv
 from .models import *
-import torch
-from pytorch_transformers import GPT2Tokenizer, GPT2LMHeadModel
+
 import pandas as pd
 import re
 
 openai.api_key = config('OPENAI_API_KEY') 
-
-model = GPT2LMHeadModel.from_pretrained("gpt2")
-model.eval()
-tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
 
 
 def get_description_string(desc_list, user_id):
@@ -23,10 +18,7 @@ def openai_tes(user_id, topic_term, matched_topics):
 
 
 def activity_score(sentence):
-    tokenize_input = tokenizer.tokenize(sentence)
-    tensor_input = torch.tensor([tokenizer.convert_tokens_to_ids(tokenize_input)])
-    loss = model(tensor_input, labels=tensor_input)
-    return -loss[0].item()
+    pass
 
 def get_single_types_activity(topic_list):
     word_list = []
@@ -49,27 +41,4 @@ def get_single_types_activity(topic_list):
 
 
 def get_activity_mask(topic_list):
-   
-    all_templates = lessonTemplates.objects.all()
-    candidates = []
-    for demos in topic_list:
-        wording = demos.item
-        candidates.append(wording)
-
-    for sentc in all_templates:
-        final_list = []
-        sent_one = sentc.wording
-        sent_template = sent_one.replace('DEMO_KS', '{}')
-
-        
-        for candidate in candidates:
-            sent_result = sent_one.replace('DEMO_KS', candidate)
-            
-            result = activity_score(sent_template.format(candidate)), sent_result, candidate
-            
-            final_list.append(result)
-
-
-
-    
-    return(final_list)
+    pass
